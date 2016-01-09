7f86ee05787ac70152948636d4c1d07eaa2c4493
SecretAccount <pasnicom@web.de>
2016-01-09 18:46:48 +0100

    WeichenRund-Methode wiederhergestellt

--- src/de/cme/GUI.java
+++ src/de/cme/GUI.java
@@ -1021,14 +1021,7 @@ public class GUI extends javax.swing.JFrame {
     }//GEN-LAST:event_jBAutomatikStartenActionPerformed
 
     private void jBWeicheRundActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBWeicheRundActionPerformed
-//        dieSteuerung.stelleWeiche(Integer.valueOf(jTFVon.getText()), 'r');
-        //Geschwindigkeit: 1000
-        int geschwindigkeitLB = 232;
-            int geschwindigkeitHB = 3;
-            int geschwindigkeit = geschwindigkeitLB;
-            geschwindigkeit += geschwindigkeitHB << 8;
-            System.out.println("Geschwindigkeit " + geschwindigkeit + " wird auf Slider gesetzt");
-//            this.setzeGeschwindigkeit(geschwindigkeit);
+        dieSteuerung.stelleWeiche(Integer.valueOf(jTFVon.getText()), 'r');
     }//GEN-LAST:event_jBWeicheRundActionPerformed
 
     private void jBWeicheGeradeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBWeicheGeradeActionPerformed
