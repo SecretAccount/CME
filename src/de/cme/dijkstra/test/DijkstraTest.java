134ee7c1c6abc6a42fe9dac9e63696604974c24d
SecretAccount <pasnicom@web.de>
2016-01-10 19:41:48 +0100

    weitere Methode hinzugefügt, um einen Weg zwischen zwei Knoten zu testen

--- src/de/cme/dijkstra/test/DijkstraTest.java
+++ src/de/cme/dijkstra/test/DijkstraTest.java
@@ -6,13 +6,21 @@ public class DijkstraTest {
 
     private Dijkstra dijkstra = new Dijkstra();
     
-    public void testeGraph() {
+    public DijkstraTest() {
         dijkstra.init();
+    }
+    
+    public void testeGraph() {
         for (int i = 1; i < 45; i++) {
             for (int j = 2; j < 45; j++) {
                 dijkstra.findeWeg(i, j);
                 dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten eingefügt sind
             }
         }
+    }
+    
+    public void testeGraph(int von, int bis) {
+        dijkstra.findeWeg(von, bis);
+        dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten eingefügt sind
     }
 }
