d7ed93b63ddcbf327420a7225c43c255831f026e
SecretAccount <pasnicom@web.de>
2016-01-13 02:17:26 +0100

    Kommentare verändert

--- src/de/cme/dijkstra/test/DijkstraTest.java
+++ src/de/cme/dijkstra/test/DijkstraTest.java
@@ -4,9 +4,10 @@ import de.cme.dijkstra.Dijkstra;
 
 public class DijkstraTest {
 
-    private Dijkstra dijkstra = new Dijkstra();
+    private Dijkstra dijkstra;
     
     public DijkstraTest() {
+        dijkstra = new Dijkstra();
         dijkstra.init();
     }
     
@@ -14,13 +15,13 @@ public class DijkstraTest {
         for (int i = 1; i < 45; i++) {
             for (int j = 2; j < 45; j++) {
                 dijkstra.findeWeg(i, j);
-                dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten eingefügt sind
+                dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
             }
         }
     }
     
     public void testeGraph(int von, int bis) {
         dijkstra.findeWeg(von, bis);
-        dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten eingefügt sind
+        dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
     }
 }
