021388d57738ffff67de54681c9b066121eab882
pascal.messner <pascal.messner@g055-01.schule.local>
2016-02-17 13:20:22 +0100

    Test des Graphen ohne Knoten 6, da entfernt

--- src/de/cme/dijkstra/test/DijkstraTest.java
+++ src/de/cme/dijkstra/test/DijkstraTest.java
@@ -5,23 +5,43 @@ import de.cme.dijkstra.Dijkstra;
 public class DijkstraTest {
 
     private Dijkstra dijkstra;
-    
+
     public DijkstraTest() {
         dijkstra = new Dijkstra();
         dijkstra.init();
     }
-    
+
     public void testeGraph() {
+        
         for (int i = 1; i < 45; i++) {
-            for (int j = 2; j < 45; j++) {
+            for (int j = 1; j < 45; j++) {
+                //Keine gleichen Knoten vergleichen
+                if(i != j && i != 6 && j != 6) {
+                dijkstra = new Dijkstra();
+                dijkstra.init();
                 dijkstra.findeWeg(i, j);
-                dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
+//                dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
+                }
             }
         }
+        
+        /*
+        //Teste Knoten 6
+        int j = 6;
+        for (int i = 1; i < 45; i++) {
+                //Keine gleichen Knoten vergleichen
+                if(i != j) {
+                dijkstra = new Dijkstra();
+                dijkstra.init();
+                dijkstra.findeWeg(j, i);
+//                dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
+                }
+        }
+        */
     }
-    
+
     public void testeGraph(int von, int bis) {
         dijkstra.findeWeg(von, bis);
-        dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
+//        dijkstra.showList(); //Zeigt Fehler, wenn noch nicht alle Knoten und Kanten im Quellcode eingefügt sind
     }
 }
